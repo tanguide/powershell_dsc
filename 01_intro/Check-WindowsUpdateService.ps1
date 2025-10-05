43d367c42e6b66a316bb6ad5b37ac0ea13da6f43
Tan Gui De <tanguide@gmail.com>
2025-10-05 15:39:36 +0800

    Second commit

---
+++ 01_intro/Check-WindowsUpdateService.ps1
@@ -0,0 +1,39 @@
+﻿[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Name,
+
+    [Parameter()]
+    [string]
+    [ValidateSet('Running','Stopped')]
+    $Status,
+
+    [Parameter()]
+    [string]
+    [ValidateSet('Automatic','Disabled','Manual')]
+    $StartupType
+
+)
+
+$service = Get-Service -Name $Name -ErrorAction Stop
+if($service.StartType -ne $StartupType)
+{
+    Write-Verbose -Message "Settinng startup type for $Name to $StartupType"
+    Set-Service -Name $Name -StartupType $StartupType
+}
+
+if($service.Status -ne $Status)
+{
+    if($service.Status -eq 'Running')
+    {
+        Write-Verbose -Message "Starting service $Name"
+        Start-Service -Name $Name
+    }
+    else 
+    {
+        Write-Verbose -Message "Stopping service $Name"
+        Stop-Service -Name $Name
+    }
+}
